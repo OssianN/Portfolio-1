44f412a67a3670c2d0450c47d3e7b7ea624c8113
OssianN <ossian.northen@gmail.com>
2021-01-19 21:51:26 +0100

    update

--- src/components/guestbookForm/index.js
+++ src/components/guestbookForm/index.js
@@ -57,7 +57,9 @@ const GuestBookForm = (props) => {
       name="guestBook"
       method="POST"
       data-netlify="true"
-      action='/guest-book'>
+      action='/guest-book'
+      data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="guestBook" />
       <button className="cancelFormButton" onClick={props.showGuestBookForm}>&#10005;</button>
       <h1>Write something for all visitors to see... or just smile and wave!</h1>
       <label htmlFor='nameInput'>Your Name</label>
