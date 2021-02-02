fa42c04682ef2f8274aa5d8052c0e63a8e1f88c5
OssianN <ossian.northen@gmail.com>
2021-02-02 12:23:23 +0100

    update

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -32,23 +32,25 @@ const Portfolio = () => {
             <a href='https://forskoleupproret.netlify.app' className='portfolioLink'>
               <h2>Förskoleupproret's Webpage</h2>
               <img src={ images.forskoleupproret } alt='forskoleupproret' className='portfolioImg'></img>
-              <p>Förskoleupproret is a non-profitable organization striving to improve the work conditions and the general environment in preschool. They asked me to build them a new webpage with their graphical profile. The serverless application is biult using React, Gatsby, Contentful, Netlify and styled with Sass.</p>
+              <p>Förskoleupproret is a non-profitable organization striving to improve the work conditions and the general environment in preschool. They asked me to build them a new webpage using their graphic profile. The serverless application is biult using React, Gatsby, Contentful, Netlify and styled with Sass.</p>
             </a>
           </FadeInSection>
           <div className='timePole'></div>
           <FadeInSection>
-            <a href='https://qutine.herokuapp.com' className='portfolioLink'>
-              <h2>Qutine</h2>
-              <img src={ images.qutine } alt='qutine' className='portfolioImg'></img>
-              <p>Qutine was created by me, <a style={{color:'#3b5249'}} href='https://github.com/jrodri06'>José Rodriguez &#x203A;</a> and <a style={{color:'#3b5249'}} href='https://github.com/tlorraine'>Therese Abrahamsson &#x203A;</a>. It's an app that helps you stay organized and keep your routine during quarantine. To create this app we utilized React, Redux, Express, MongoDB, Mongoose, Socket.io and Heruko. Only developed for desktop.</p>
-            </a>
+            <div className='portfolioLink'>
+              <a href='https://qutine.herokuapp.com'>
+                <h2>Qutine</h2>
+                <img src={ images.qutine } alt='qutine' className='portfolioImg'></img>
+              </a>
+                <p>Qutine was created by me, <a style={{color:'#3b5249'}} href='https://github.com/jrodri06'>José Rodriguez &#x203A;</a> and <a style={{color:'#3b5249'}} href='https://github.com/tlorraine'>Therese Abrahamsson &#x203A;</a>. It's an app that helps you stay organized and keep your routine during quarantine. To create this app we utilized React, Redux, Express, MongoDB, Mongoose, Socket.io and Heruko. Only developed for desktop.</p>
+            </div>
           </FadeInSection>
           <div className='timePole'></div>
           <FadeInSection>
             <a href='https://github.com/OssianN/GalleryApp-React' className='portfolioLink'>
               <h2>Gallery App</h2>
               <img src={images.gallery} alt='gallery' className='portfolioImg'></img>
-              <p>During the &lt;/salt&gt; bootcamp, me and my mob built a lot of different projects, mobprogramming. This is one of them – a gallery app that works against the unsplashed API. It's built with react in the frontend and express in the backend. We havn't deployed it anyware, but check out the repo if you want.</p>
+              <p>During the &lt;/salt&gt; bootcamp, me and my mob built a lot of different projects, mobprogramming. This is one of them – a gallery app that works against the unsplashed API. It's built with React in the frontend and express in the backend. We havn't deployed it anyware, but check out the repo if you want.</p>
             </a>
           </FadeInSection>
           <div className='timePole'></div>
