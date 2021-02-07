c66505a8090a5db3334763a97196a690ca019ad1
OssianN <ossian.northen@gmail.com>
2021-02-07 13:44:11 +0100

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,10 +32,8 @@ const Nav = props => {
 
   const checkWindowAndRef = () => {
     if (window === 'undefined' && !navbarRef.current.style) {
-      console.log('window');
       return false;
     }
-    console.log(navbarRef.current);
     return true;
   }
 
