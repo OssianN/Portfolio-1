1febb3e9a56054c3accb809e17aa3b60ee4da0fa
OssianN <ossian.northen@gmail.com>
2021-02-11 19:58:59 +0100

    update

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,21 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'gatsby';
 import '../styles/navbar.scss';
 
 const Navigation = props => {
+  const [scrollAtBottom, setScrollAtBottom] = useState(false)
   const navbarRef = useRef(null);
 
   const isWindowAndNavUndefined = () => window === 'undefined' || !navbarRef.current;
 
   const changeNavBackground = () => {
     if (isWindowAndNavUndefined()) return;
-
     if ( window.scrollY > 0 ) {
       navbarRef.current.style.backgroundColor = '#fcdfa6f6';
     } else {
       navbarRef.current.style.backgroundColor = 'transparent';
     }
   }
+
+//   const handleScroll = () => {
+//     const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+//     const body = document.body;
+//     const html = document.documentElement;
+//     const docHeight = Math.max(body.scrollHeight, document.body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+//     // windowBottom >= docHeight
+//       // setScrollAtBottom(true)
+// }
 
   const hideNavOnScroll = () => {
     if (isWindowAndNavUndefined()) return;
@@ -24,7 +33,10 @@ const Navigation = props => {
       if (isWindowAndNavUndefined()) return;
       changeNavBackground();
       const currentScrollY = window.scrollY;
-      if (prevScrollY < currentScrollY && currentScrollY > 50) {
+      const windowBottom = window.innerHeight + window.pageYOffset;
+      const bodyHeight = document.documentElement.scrollHeight;
+      console.log(windowBottom, bodyHeight)
+      if (prevScrollY < currentScrollY && currentScrollY > 50 || windowBottom >= bodyHeight) {
         navbarRef.current.classList.add('scrollNav');
       } else {
         navbarRef.current.classList.remove('scrollNav');
